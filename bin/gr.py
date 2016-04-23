429fc16f03c8d918110f88d0f62e0894546a7790
Tom Dymond <tom@kmem.org>
2016-04-23 22:58:27 +0100

    fixes

--- bin/gr.py
+++ bin/gr.py
@@ -13,7 +13,6 @@ a = Wifishit(camera_ssid=os.environ["RICOH_SSID"])
 if a.is_camera_on():
   if a.connect_to_camera_ssid():
     b = Grimage()
-    dirs = b.listdirs()
     for d in b.listdirs():
       for i in b.listimages(d):
         for j in i:
