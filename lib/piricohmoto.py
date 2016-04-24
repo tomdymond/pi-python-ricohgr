98feea6c21219bef9b117277e5dc91f9edf71fde
Tom Dymond <tom@kmem.org>
2016-04-24 10:07:54 +0100

    Fix

--- lib/piricohmoto.py
+++ lib/piricohmoto.py
@@ -39,13 +39,13 @@ class Grimage(object):
       print "Skipping {}. Already downloaded".format(filename)
       return True
     try:
-      timestamp_1 = datetime.datetime.now().strftime('%s')
+      timestamp_1 = int(datetime.datetime.now().strftime('%s'))
       r = requests.get('http://{ip}/v1/photos/{dirname}/{filename}?size={size}'.format(ip=self.ip, dirname=dirname, filename=filename, size=size), timeout=10)
       with open('{}/{}'.format(DOWNLOAD_DIR, filename), 'wb') as f:
         for chunk in r.iter_content(chunk_size=1024): 
           if chunk: # filter out keep-alive new chunks
             f.write(chunk)
-      timestamp_2 = datetime.datetime.now().strftime('%s')
+      timestamp_2 = int(datetime.datetime.now().strftime('%s'))
       self.update_state(filename)
       print "Download took {} seconds".format(timestamp_2 - timestamp_1)
       return True
