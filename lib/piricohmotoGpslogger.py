d8fdaf6a16b38c3d212fbee03d178342b3f9ae46
Tom Dymond <tom@kmem.org>
2016-04-27 07:48:13 +0100

    test

--- lib/piricohmotoGpslogger.py
+++ lib/piricohmotoGpslogger.py
@@ -1,18 +1,4 @@
 #! /usr/bin/python
 
-from gps import *
-from time import *
-import time
-import threading
 
-class GpsPoller(threading.Thread):
-  def __init__(self):
-    threading.Thread.__init__(self)
-    self.gpsd = gps(mode=WATCH_ENABLE) #starting the stream of info
-    self.current_value = None
-    self.running = True #setting the thread running to true
-
-  def run(self):
-    while self.running:
-      self.gpsd.next() #this will continue to loop and grab EACH set of gpsd info to clear the buffer
 
--- lib/piricohmotoLogger.py
+++ lib/piricohmotoLogger.py
@@ -10,6 +10,10 @@ from daemon import runner
 from os import sys, path
 import datetime
 import csv
+from gps import *
+from time import *
+import time
+import threading
 
 cwd = path.dirname(path.abspath(__file__))
 sys.path.append('{}/lib/'.format(cwd))
@@ -17,6 +21,17 @@ sys.path.append('{}/../lib/'.format(cwd))
 from piricohmotoGpslogger import GpsPoller
 
 LOGGER_FILE='/tmp/gpslog'
+
+class GpsPoller(threading.Thread):
+  def __init__(self):
+    threading.Thread.__init__(self)
+    self.gpsd = gps(mode=WATCH_ENABLE) #starting the stream of info
+    self.current_value = None
+    self.running = True #setting the thread running to true
+
+  def run(self):
+    while self.running:
+      self.gpsd.next() #this will continue to loop and grab EACH set of gpsd info to clear the buffer
 
 class App():
   def __init__(self, sleep_time=5):
