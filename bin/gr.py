8304c35b69fa0545377d49d37eea32a826166b26
Tom Dymond <tom@kmem.org>
2016-05-02 17:38:20 +0100

    Trying something

--- bin/gr.py
+++ bin/gr.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import os
+import threading
 
 cwd = os.path.dirname(os.path.abspath(__file__))
 os.sys.path.append('{}/lib/'.format(cwd))
@@ -8,12 +9,28 @@ os.sys.path.append('{}/../lib/'.format(cwd))
 
 from piricohmotoRicoh import Ricoh
 
-flow = Ricoh(config_file='/etc/piricohmoto.yml')
-connection = flow.connection()
+class Download_all(threading.Thread)
+  def __init__(self, flow):
+    threading.Thread.__init__(self)
 
-if connection.is_camera_on():
-  if connection.connect_to_camera_ssid():
+  def run(self):
+    """ Make it start """
     flow.download_all()
 
-  flow.geotag_all()
-  flow.upload_all()
+class Tag_all(threading.Thread)
+  def __init__(self, flow):
+    threading.Thread.__init__(self)
+
+  def run(self):
+    """ Make it start """
+    flow.geotag_all()
+
+flow = Ricoh(config_file='/etc/piricohmoto.yml')
+
+if flow.connection().is_camera_on():
+  if flow.connection().connect_to_camera_ssid():
+    Download_all(flow)
+    Tag_all(flow)
+
+
+#  flow.upload_all()
