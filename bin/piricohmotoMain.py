81e1a18a5062f47b29927116711c0b43226f64de
tomdymond <tom@kmem.org>
2016-05-11 20:22:35 +0100

    Update piricohmotoMain.py

--- bin/piricohmotoMain.py
+++ bin/piricohmotoMain.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 """ 
-Service for logging gps coordinates
-Store the GPS coordinates in Redis under the hkey GPS for retreival later. 
+Main service 
 """
 
 import logging
@@ -10,13 +9,45 @@ from daemon import runner
 from os import sys, path, mkdir
 import datetime
 from time import *
-import time
-import redis
-import json
+import os
+import threading
+from piricohmotoRicoh import Ricoh
 
 cwd = path.dirname(path.abspath(__file__))
 sys.path.append('{}/lib/'.format(cwd))
 sys.path.append('{}/../lib/'.format(cwd))
+
+class Download_all(threading.Thread):
+  def __init__(self, flow):
+    print "download all "
+    threading.Thread.__init__(self)
+    self.flow = flow
+    self.running = True
+
+  def run(self):
+    """ Make it start """
+    print "make it start"
+    self.flow.download_all()
+
+class Tag_all(threading.Thread):
+  def __init__(self, flow):
+    threading.Thread.__init__(self)
+    self.flow = flow
+    self.running = True
+
+  def run(self):
+    """ Make it start """
+    self.flow.geotag_all()
+
+class Upload_all(threading.Thread):
+  def __init__(self, flow):
+    threading.Thread.__init__(self)
+    self.flow = flow
+    self.running = True
+
+  def run(self):
+    """ Make it start """
+    self.flow.upload_all()
 
 
 class App():
@@ -32,6 +63,30 @@ class App():
             
   def run(self):
     while True:
+      
+      flow = Ricoh(config_file='/config/piricohmoto.yml')
+      conn = flow.connection()
+      
+      try:
+        b = Tag_all(flow)
+        b.start()
+      
+        if conn.is_camera_on():
+          if conn.connect_to_camera_ssid():
+            a = Download_all(flow)
+            a.start()
+        else:
+          c = Upload_all(flow)
+          c.start()
+      
+      except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
+        print ("\nKilling Threads...")
+        a.running = False
+        b.running = False
+        a.join() # wait for the thread to finish what it's doing
+        b.join() # wait for the thread to finish what it's doing
+      print ("Done.\nExiting.")      
+
       logger.debug("Debug message")
       logger.info("Info message")
       logger.warn("Warning message")
