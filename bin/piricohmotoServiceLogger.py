87e9683c94648f1efc8f41118e2eadfaf5bd4d92
tomdymond <tom@kmem.org>
2016-05-12 22:04:07 +0100

    Update piricohmotoServiceLogger.py

--- bin/piricohmotoServiceLogger.py
+++ bin/piricohmotoServiceLogger.py
@@ -8,7 +8,6 @@ Store the GPS coordinates in Redis under the hkey GPS for retreival later.
 
 import logging
 import time
-from daemon import runner
 from os import sys, path, mkdir
 import datetime
 from gps import *
@@ -22,7 +21,6 @@ cwd = path.dirname(path.abspath(__file__))
 sys.path.append('{}/lib/'.format(cwd))
 sys.path.append('{}/../lib/'.format(cwd))
 
-
 class GpsPoller(threading.Thread):
   def __init__(self):
     threading.Thread.__init__(self)
@@ -34,54 +32,20 @@ class GpsPoller(threading.Thread):
     while self.running:
       self.gpsd.next() #this will continue to loop and grab EACH set of gpsd info to clear the buffer
 
-class App():
-  def __init__(self, sleep_time=5):
-    self.stdin_path = '/dev/null'
-    self.stdout_path = '/dev/stdout'
-    self.stderr_path = '/dev/stderr'
-    if not path.exists('/var/run/piricohmoto/'):
-      mkdir('/var/run/piricohmoto/')
-    self.pidfile_path =  '/var/run/piricohmoto/piricohmotoGpslogger.pid'
-    self.pidfile_timeout = 5
-    self.sleep_time = sleep_time
-            
-  def run(self):
-    gpsp = GpsPoller() # create the thread
-    r = redis.StrictRedis(host='localhost')
-    try:
-      gpsp.start() # start it up
-      while True:
-        if gpsp.gpsd.fix.latitude:
-          localtime = datetime.datetime.now().strftime('%s')
-          r.hmset('GPS', {localtime: json.dumps(gpsp.gpsd.fix.__dict__)})
+gpsp = GpsPoller() # create the thread
+r = redis.StrictRedis(host='localhost')
+try:
+  gpsp.start() # start it up
+  while True:
+    if gpsp.gpsd.fix.latitude:
+      localtime = datetime.datetime.now().strftime('%s')
+      r.hmset('GPS', {localtime: json.dumps(gpsp.gpsd.fix.__dict__)})
 
-        logger.debug("Debug message")
-        logger.info("Info message")
-        logger.warn("Warning message")
-        logger.error("Error message")
-        time.sleep(self.sleep_time)
+    time.sleep(5)
 
-    except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
-      print ("\nKilling Thread...")
-      gpsp.running = False
-      gpsp.join() # wait for the thread to finish what it's doing
-    print ("Done.\nExiting.")
+except (KeyboardInterrupt, SystemExit): #when you press ctrl+c
+  print ("\nKilling Thread...")
+  gpsp.running = False
+  gpsp.join() # wait for the thread to finish what it's doing
+print ("Done.\nExiting.")
 
-if __name__ == "__main__":
-  if len(sys.argv) > 2:
-    sleep_time = int(sys.argv[2])
-  else:
-    sleep_time = 5
-  app = App(sleep_time=sleep_time)
-  logger = logging.getLogger("DaemonLog")
-  logger.setLevel(logging.INFO)
-  formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-  handler = logging.FileHandler("/var/log/piricohmoto.log")
-  handler.setFormatter(formatter)
-  logger.addHandler(handler)
-
-  daemon_runner = runner.DaemonRunner(app)
-
-  #This ensures that the logger file handle does not get closed during daemonization
-  daemon_runner.daemon_context.files_preserve=[handler.stream]
-  daemon_runner.do_action()
