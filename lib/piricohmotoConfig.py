9fb468c98b0337031bc3624dc502fe464e1ced44
Tom Dymond <tom@kmem.org>
2016-05-13 10:17:33 +0100

    wip

--- lib/piricohmotoConfig.py
+++ lib/piricohmotoConfig.py
@@ -6,25 +6,6 @@ import os
 import sys
 
 
-class piglowclient(object):
-  def __init__(self):
-    pass
-  
-  def notify(self, colour, duration=5, flashing=False):
-    """ Send a request to the notificatin daemon """
-    if flashing:
-      c = 'flashing'
-    else:
-      c = 'fixed'
-    try:
-      response = requests.get('http://127.0.0.1/piglow/{}/{}/{}'.format(colour, c, duration))
-      if response.status_code == 200:
-        return True
-      return False
-    except Exception as e:
-      return False
-    
-
 class Config(object):
   def __init__(self, **kwargs):
     self.config = self.load_config(kwargs['config_file'])
