c9ee80e293955a7d0564c567a9eb92eff702cde7
tomdymond <tom@kmem.org>
2016-05-13 08:53:05 +0100

    Update start.sh

--- Docker/start.sh
+++ Docker/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 chown -R redis:redis /var/lib/redis
-chown -R appuser /var/log/piricohmoto
+chown -R appuser /var/log/piricohmoto /download
 
 service redis-server start
 service supervisor start
