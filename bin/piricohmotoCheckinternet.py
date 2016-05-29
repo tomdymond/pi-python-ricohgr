fda3b23881d6554a0858fb304041681bdcfc062a
Tom Dymond <tom@kmem.org>
2016-05-29 10:03:56 +0100

    wip

--- bin/piricohmotoCheckinternet.py
+++ bin/piricohmotoCheckinternet.py
@@ -13,19 +13,20 @@ from piricohmotoNotifier import Notifier
 
 
 def check_internet():
-    response = requests.head('http://www.google.com')
     n = Notifier()
     n.power=50
 
-    if int(response.status_code) in (200, 302):
-        n.flashing=0
-    else:
+    try:
+        response = requests.head('http://www.google.com')
+        if int(response.status_code) in (200, 302):
+            n.flashing=0
+        else:
+            n.flashing=1
+    except Exception as e:
         n.flashing=1
 
-    print response.status_code
     n.duration=40
     n.led([n.led_codes['white'][0]])
-    print n.payload
     n.send()
 
 while True:
