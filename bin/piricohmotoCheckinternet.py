644a31545daf442c97fd4a01f5321e869451fafa
Tom Dymond <tom@kmem.org>
2016-05-29 09:25:00 +0100

    wip

--- bin/piricohmotoCheckinternet.py
+++ bin/piricohmotoCheckinternet.py
@@ -3,6 +3,7 @@
 
 from os import sys, path, mkdir
 import requests
+from time import sleep
 
 cwd = path.dirname(path.abspath(__file__))
 sys.path.append('{}/lib/'.format(cwd))
@@ -10,16 +11,22 @@ sys.path.append('{}/../lib/'.format(cwd))
 
 from piricohmotoNotifier import Notifier
 
-response = requests.get('http://www.google.com')
+response = requests.head('http://www.google.com')
 n = Notifier()
 n.power=50
 n.flashing=1
 n.duration=2
 
 if int(response.status_code) == 200:
-    n.flashing=1
-else:
     n.flashing=0
 
+else:
+    n.flashing=1
+
 n.led([n.led_codes['white'][0]])
+n.duration=20
+ 
+
 n.send()
+
+sleep(30)
