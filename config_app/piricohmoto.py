35bd360a7c9c50557c6f642d572d9b1eb0f2955e
Tom Dymond <tom@kmem.org>
2016-05-30 15:59:26 +0100

    wip

--- config_app/piricohmoto.py
+++ config_app/piricohmoto.py
@@ -2,18 +2,30 @@
 
 from flask import Flask, render_template, make_response
 from flask_bootstrap import Bootstrap
+import os
+
 
 def create_app():
   app = Flask(__name__)
   Bootstrap(app)
   return app
 
+def remove_ext(item):
+  base = item.split('.')
+  ext = base[1]
+  s = base[0].split('_')
+  return s[0]
+
 app = create_app()
 
 @app.route("/")
 def hello():
-  return render_template('test.html',testvar='foo')
+  images = os.listdir('/download')
+
+  foo = map(remove_ext, images)
+
+  return render_template('test.html', images=foo)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=8080)
-
+  
