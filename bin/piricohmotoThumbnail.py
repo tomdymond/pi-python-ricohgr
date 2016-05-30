4b18e9e0c7f58c4240c1fc2a19b35f79de771ec0
Tom Dymond <tom@kmem.org>
2016-05-30 12:06:40 +0100

    wip

---
+++ bin/piricohmotoThumbnail.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+""" 
+Thumbnail script 
+"""
+
+from os import sys, path, mkdir
+
+cwd = path.dirname(path.abspath(__file__))
+sys.path.append('{}/lib/'.format(cwd))
+sys.path.append('{}/../lib/'.format(cwd))
+
+from piricohmotoCamera import Camera
+
+flow = Camera(config_file='/config/piricohmoto.yml')
+while True:
+  flow.thumbnail_all()
+  sleep(30)
+  
